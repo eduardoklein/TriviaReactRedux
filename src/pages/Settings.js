1290816941d1cd76b07f6b5ff35c27c3dcb269ac
Matheus Boechat <matheusfgb@gmail.com>
2023-05-22 17:09:03 -0300

    req3

---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <h2 data-testid="settings-title">Configurações</h2>
+      </div>
+    );
+  }
+}
+export default Settings;
